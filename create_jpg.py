f23408bfeaa5369fd7a53d52791706be4603a99f
Do1kk <do.siekierskipd@gmail.com>
2020-03-29 13:55:53 +0200

    Loop in create_jpg

--- create_jpg.py
+++ create_jpg.py
@@ -1,6 +1,12 @@
 from numpy import genfromtxt
 import imageio
 
-file_name = 100
-data_array = genfromtxt("csv_files/" + str(file_name) + ".csv", delimiter=";")
-imageio.imwrite("images_from_csv/outfile.jpg", data_array)
+record_names = "mit-bih/RECORDS"
+
+# Reading all record names from the file.
+with open(record_names) as file:
+    for line in file:
+        record_name = str(line.strip())
+        data_array = genfromtxt("csv_files/" + record_name + ".csv", delimiter=";")
+        imageio.imwrite("images_from_csv/" + record_name + ".jpg", data_array)
+        print(f"Create image from csv file: {record_name}.jpg")
