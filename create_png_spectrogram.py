be5a1e5a16508eadfa281a870daa0e0f5af15381
Do1kk <do.siekierskipd@gmail.com>
2020-04-11 14:59:15 +0200

    Using "multiprocessing" to create photos

--- create_png_spectrogram.py
+++ create_png_spectrogram.py
@@ -1,8 +1,12 @@
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+import multiprocessing
 
 
+start_time = time.time()
+images_folder = "images/"
 record_name = "type_A"
 beat_ann = (
     "N",  # Normal beat.
@@ -22,11 +26,49 @@ beat_ann_file = (
 )
 beat_ann_dict = dict(zip(beat_ann, beat_ann_file))
 y = genfromtxt("csv_type_files/" + record_name + ".csv", delimiter=";")
-x = np.array(y)
+podzielnik = int(len(y) / 3)
+x = np.array(y[:podzielnik])
+xx = np.array(y[podzielnik : podzielnik * 2])
+xxx = np.array(y[podzielnik * 2 :])
 
-# Creating a figure so that the image has dimensions of 1024x1024.
-plt.figure(figsize=(10.54, 10.54))
-spectrum, freqs, t, im = plt.specgram(x, Fs=360)
-plt.axis("off")
-plt.tight_layout()
-plt.savefig("pict.png", bbox_inches="tight", pad_inches=0, dpi=100)
+
+def save_image(x, step):
+    for i, data in enumerate(x):
+        # Creating a figure so that the image has dimensions of 1024x1024.
+        plt.figure(figsize=(5.42, 5.42))
+        spectrum, freqs, t, im = plt.specgram(data, Fs=360)
+        plt.axis("off")
+        plt.tight_layout()
+        plt.savefig(
+            f"{images_folder}{record_name}/{i + step}.png",
+            bbox_inches="tight",
+            pad_inches=0,
+            dpi=100,
+        )
+        plt.close()
+
+
+if __name__ == "__main__":
+    step = 0
+    # creating processes
+    p1 = multiprocessing.Process(target=save_image, args=(x, step,))
+    step += podzielnik
+    p2 = multiprocessing.Process(target=save_image, args=(xx, step,))
+    step += podzielnik
+    p3 = multiprocessing.Process(target=save_image, args=(xxx, step,))
+
+    # starting process 1
+    p1.start()
+    # starting process 2
+    p2.start()
+    # starting process 3
+    p3.start()
+
+    # wait until process 1 is finished
+    p1.join()
+    # wait until process 2 is finished
+    p2.join()
+    # wait until process 3 is finished
+    p3.join()
+
+    print("--- %s seconds ---" % (time.time() - start_time))
