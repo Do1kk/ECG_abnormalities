163330f37bf5aac1b5bbd527f5d937576dc9d44d
Do1kk <do.siekierskipd@gmail.com>
2020-06-17 12:05:13 +0200

    Some classification model

--- image_split.py
+++ image_split.py
@@ -2,16 +2,19 @@ import split_folders
 
 input_folder = "images/"
 
-# split_folders.ratio(
-#     input_folder, output=input_folder + "train_val_test", seed=43, ratio=(0.8, 0.1, 0.1)
-# )
-
-# Split val/test with a fixed number of items e.g. 100 for each set.
-# oversample <- można użyć gdy dane nie są zrównoważone
-split_folders.fixed(
+split_folders.ratio(
     input_folder,
     output=input_folder + "train_val_test",
     seed=43,
-    fixed=(400, 400),
-    oversample=False,
+    ratio=(0.7, 0.15, 0.15),
 )
+
+# Split val/test with a fixed number of items e.g. 100 for each set.
+# oversample <- można użyć gdy dane nie są zrównoważone
+# split_folders.fixed(
+#     input_folder,
+#     output=input_folder + "train_val_test",
+#     seed=43,
+#     fixed=(400, 400),
+#     oversample=False,
+# )
