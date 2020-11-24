e40d78428afd179a5a46ce6aa5ac87463b967d5c
Do1kk <do.siekierskipd@gmail.com>
2020-11-24 21:16:14 +0100

    Unittests example

---
+++ tests/test_csv_ann_type.py
@@ -0,0 +1,26 @@
+import unittest
+import os
+import sys
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))  # Change dir to parent.
+from csv_ann_type import csv_folder
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+
+    def test_isupper(self):
+        self.assertTrue("FOO".isupper())
+        self.assertFalse("Foo".isupper())
+
+    def test_split(self):
+        s = "hello world"
+        self.assertEqual(s.split(), ["hello", "world"])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
+if __name__ == "__main__":
+    unittest.main()
